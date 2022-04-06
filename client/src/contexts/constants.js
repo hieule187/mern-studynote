5e89d00fbbbe2ea2c4ff3cdb5a35c375abeffa19
hieule187 <1851171491@e.tlu.edu.vn>
2022-04-06 14:03:52 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:5000/api'
-    : 'somedeployURL';
+    : 'https://server-studynote.herokuapp.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'study_Note';
 
